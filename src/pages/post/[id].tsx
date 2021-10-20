b4e59c60ac2b18dcd44e2bbd2077ee49c40dc73b
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-10-20 17:10:07 +0900

    create postDetail page.

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import styled from "styled-components";
+import Layout from "../../components/Layout";
 
-const PostDetail = () => {
-  return <div>PostDetail</div>;
-}
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1.4;
+`;
+
+const PostDetail = ({ data }) => {
+  return (
+    <Layout>
+      <main>
+        <Title>{data.name}</Title>
+      </main>
+    </Layout>
+  );
+};
 
 export default PostDetail;
+
+export const getServerSidePaths = async () => {
+  const fetchData = await fetch(
+    `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&large_area=Z011`
+  )
+    .then(res => res.json())
+    .catch(() => null);
+  const paths = fetchData.results.shop.map(
+    fetchData => `/posts/${fetchData.id}`
+  );
+  return { paths, fallback: false };
+};
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const fetchData = await fetch(
+    `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&large_area=Z011`
+  )
+    .then(res => res.json())
+    .catch(() => null);
+  const data = fetchData.results.shop.find(fetchData => fetchData.id == id);
+  return {
+    props: {
+      data,
+    },
+  };
+}
