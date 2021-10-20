e71bf2f73958a3c4e55442f593d49c3bf1c9bc1f
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-10-20 23:37:04 +0900

    fix header layout.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Header = () => {
+  const router = useRouter();
+  const pathname = router.pathname;
+  const otherTopPage = pathname != "/"; // toppage以外
+
+  const searchFunc = () => {
+    console.log("searchFunc");
+  }
+
   return (
     <HeaderLayout>
-      <Link href="/" passHref as="/">
-        <a>戻る</a>
-      </Link>
+      {otherTopPage && (
+        <Link href="/" passHref as="/">
+          <ReturnText>戻る</ReturnText>
+        </Link>
+      )}
       <Title>Tabetai</Title>
+      <SearchText onClick={searchFunc}>検索</SearchText>
     </HeaderLayout>
   );
 };
@@ -24,8 +36,24 @@ const HeaderLayout = styled.header`
   background: #333;
   z-index: 1;
 `;
+const ReturnText = styled.a`
+  position: absolute;
+  top: 50%;
+  left: 15px;
+  transform: translate(0, -50%);
+  font-size: 14px;
+  color: #fff;
+`;
 const Title = styled.h1`
   font-size: 22px;
+  color: #fff;
+`;
+const SearchText = styled.p`
+  position: absolute;
+  top: 50%;
+  right: 15px;
+  transform: translate(0, -50%);
+  font-size: 14px;
   color: #fff;
 `;
 
