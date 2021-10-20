05215051b693581d9abb20744bfd53ee4251cccb
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-10-21 01:06:44 +0900

    fix layout in header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Header = () => {
+  const [text, setText] = useState<string>("");
   const router = useRouter();
   const pathname = router.pathname;
   const otherTopPage = pathname != "/"; // toppage以外
+  const onlyTopPage = pathname == "/"; // toppageのみ
 
-  const searchFunc = () => {
-    console.log("searchFunc");
-  }
+  const filterFunc = () => {
+    console.log("filterFunc");
+  };
+
+  const handleInput = e => {
+    setText(e.target.value);
+  };
 
   return (
     <HeaderLayout>
@@ -19,7 +26,15 @@ const Header = () => {
         </Link>
       )}
       <Title>Tabetai</Title>
-      <SearchText onClick={searchFunc}>検索</SearchText>
+      {onlyTopPage && (
+        <>
+          <FilterText onClick={filterFunc}>絞り込む</FilterText>
+          <Form>
+            <SearchLabel htmlFor="search">検索キーワード:</SearchLabel>
+            <Input id="search" type="text" value={text} onChange={handleInput} />
+          </Form>
+        </>
+      )}
     </HeaderLayout>
   );
 };
@@ -48,13 +63,31 @@ const Title = styled.h1`
   font-size: 22px;
   color: #fff;
 `;
-const SearchText = styled.p`
+const FilterText = styled.p`
   position: absolute;
   top: 50%;
   right: 15px;
   transform: translate(0, -50%);
   font-size: 14px;
   color: #fff;
+`;
+const Form = styled.form`
+  padding: 10px 5px;
+  position: absolute;
+  background: #fff;
+  border-bottom: 2px solid #333;
+  text-align: center;
+  bottom: -42px;
+  left: 0;
+  width: 100%;
+`;
+const Input = styled.input`
+  border: 1px solid #333;
+  font-size: 14px;
+`;
+const SearchLabel = styled.label`
+  margin-right: 10px;
+  font-size: 14px;
 `;
 
 export default Header;
